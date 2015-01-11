7ba30a6c25d637cd565abd6dc6f57ecf46f8f1d1
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-01-11 17:39:04 -0500

    Initial Commit

---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by HUGE | Isaiah Belle on 1/11/15.
+//  Copyright (c) 2015 IsaiahBelleDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
