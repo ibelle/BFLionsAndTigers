becaaec647121f4e3d5c97f2b093e59f0dd4d803
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-01-12 21:52:11 -0500

    Array Refactoring
    Array Refactoring

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var breedLabel: UILabel!
     var myTigers: [Tiger]!
-    
+       
     
     
     override func viewDidLoad() {
@@ -46,18 +46,18 @@ class ViewController: UIViewController {
         var tigerCollection:[Tiger] = [Tiger]()
         
         var tiger1 = Tiger(age: 5, name: "Tony", breed: "Bengal", image:UIImage(named: "BengalTiger.jpg"))
-        tigerCollection.append(tiger1)
+        tigerCollection += [tiger1]
         
         var tiger2 = Tiger(age: 6, name: "Tigger", breed: "Indochinese Tiger", image:UIImage(named: "IndochineseTiger.jpg"))
-        tigerCollection.append(tiger2)
+        tigerCollection += [tiger2]
         
         
         var tiger3 = Tiger(age: 7, name: "Jacob", breed: "Siberian Tiger", image:UIImage(named: "SiberianTiger.jpg"))
-        tigerCollection.append(tiger3)
+         tigerCollection += [tiger3]
         
         
         var tiger4 = Tiger(age: 8, name: "Carla", breed: "Malayan Tiger", image:UIImage(named: "MalayanTiger.jpg"))
-        tigerCollection.append(tiger4)
+          tigerCollection += [tiger4]
         
         return tigerCollection;
     }
