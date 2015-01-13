31ee69322ae03378e6991719b6476cc43c160f6f
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-01-12 21:28:34 -0500

    With Arrays1
    With Arrays1

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -9,67 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var breedLabel: UILabel!
+    var myTigers: [Tiger]!
     
-   
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        var myTigers = buildTigerCollection()
+        myTigers = buildTigerCollection()
         
         println(myTigers)
         
-        for (name, tiger) in myTigers {
-            println("My Tiger is Named \(name) his breed is \(tiger.breed)")
+        for (tiger) in myTigers {
+            println("My Tiger is Named \(tiger.name) his breed is \(tiger.breed)")
         }
         
         
-        setCurrentTiger(myTigers["Tony"]!)
+        setCurrentTiger(myTigers.first!)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
-        
+       myTigers.generate()
     }
     
-    func buildTigerCollection() -> [String: Tiger]{
-        var tigerCollection:[String: Tiger] = [String: Tiger]()
+    func buildTigerCollection() -> [Tiger]{
+        var tigerCollection:[Tiger] = [Tiger]()
         
         var tiger1 = Tiger(age: 5, name: "Tony", breed: "Bengal", image:UIImage(named: "BengalTiger.jpg"))
-        tigerCollection[tiger1.name] = tiger1
-   
+        tigerCollection.append(tiger1)
+        
         var tiger2 = Tiger(age: 6, name: "Tigger", breed: "Indochinese Tiger", image:UIImage(named: "IndochineseTiger.jpg"))
-        tigerCollection[tiger2.name] = tiger2
-       
+        tigerCollection.append(tiger2)
+        
         
         var tiger3 = Tiger(age: 7, name: "Jacob", breed: "Siberian Tiger", image:UIImage(named: "SiberianTiger.jpg"))
-        tigerCollection[tiger3.name] = tiger3
+        tigerCollection.append(tiger3)
         
         
         var tiger4 = Tiger(age: 8, name: "Carla", breed: "Malayan Tiger", image:UIImage(named: "MalayanTiger.jpg"))
-        tigerCollection[tiger4.name] = tiger4
- 
-
+        tigerCollection.append(tiger4)
+        
         return tigerCollection;
     }
+    
     func setCurrentTiger(myTiger: Tiger){
         myImageView.image = myTiger.image
         nameLabel.text = myTiger.name
         var tigerAge=myTiger.age
         ageLabel.text = "\(tigerAge)"
         breedLabel.text = myTiger.breed
-
+        
     }
-
+    
 }
 
